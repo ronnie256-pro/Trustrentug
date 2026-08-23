23c96f3239a4ee8429b5fe8865612fd05556b9ef
qodo <qodo@google.com>
2026-08-23 09:37:30 +0300

    add ContentSecurityPolicyMiddleware to allow cloudflare insights and recaptcha scripts

---
+++ core/middleware.py
@@ -0,0 +1,32 @@
+class ContentSecurityPolicyMiddleware:
+    """
+    Custom Middleware to enforce comprehensive Content Security Policy headers,
+    permitting Cloudflare Insights, Google ReCAPTCHA, FontAwesome CDN, Leaflet, and static assets.
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        
+        csp_script_src = (
+            "'self' 'unsafe-inline' 'unsafe-eval' "
+            "https://www.google.com/recaptcha/ "
+            "https://www.gstatic.com/recaptcha/ "
+            "https://cdnjs.cloudflare.com "
+            "https://static.cloudflareinsights.com "
+            "https://cloudflareinsights.com"
+        )
+        
+        csp_policy = (
+            f"default-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob: https:; "
+            f"script-src {csp_script_src}; "
+            f"script-src-elem {csp_script_src}; "
+            "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com; "
+            "font-src 'self' data: https://cdnjs.cloudflare.com https://fonts.gstatic.com; "
+            "img-src 'self' data: blob: https:; "
+            "connect-src 'self' https://static.cloudflareinsights.com https://cloudflareinsights.com https://www.google.com/recaptcha/ https:;"
+        )
+        
+        response['Content-Security-Policy'] = csp_policy
+        return response
--- core/settings.py
+++ core/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'core.middleware.ContentSecurityPolicyMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
