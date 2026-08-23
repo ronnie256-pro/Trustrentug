ab41add2de330d176739e34fb1bcf73824e1fc07
qodo <qodo@google.com>
2026-08-23 11:26:47 +0300

    add safety check to ContentSecurityPolicyMiddleware

--- core/middleware.py
+++ core/middleware.py
@@ -8,25 +8,25 @@ class ContentSecurityPolicyMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
-        
-        csp_script_src = (
-            "'self' 'unsafe-inline' 'unsafe-eval' "
-            "https://www.google.com/recaptcha/ "
-            "https://www.gstatic.com/recaptcha/ "
-            "https://cdnjs.cloudflare.com "
-            "https://static.cloudflareinsights.com "
-            "https://cloudflareinsights.com"
-        )
-        
-        csp_policy = (
-            f"default-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob: https:; "
-            f"script-src {csp_script_src}; "
-            f"script-src-elem {csp_script_src}; "
-            "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com; "
-            "font-src 'self' data: https://cdnjs.cloudflare.com https://fonts.gstatic.com; "
-            "img-src 'self' data: blob: https:; "
-            "connect-src 'self' https://static.cloudflareinsights.com https://cloudflareinsights.com https://www.google.com/recaptcha/ https:;"
-        )
-        
-        response['Content-Security-Policy'] = csp_policy
+        if response is not None and hasattr(response, '__setitem__'):
+            csp_script_src = (
+                "'self' 'unsafe-inline' 'unsafe-eval' "
+                "https://www.google.com/recaptcha/ "
+                "https://www.gstatic.com/recaptcha/ "
+                "https://cdnjs.cloudflare.com "
+                "https://static.cloudflareinsights.com "
+                "https://cloudflareinsights.com"
+            )
+
+            csp_policy = (
+                f"default-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob: https:; "
+                f"script-src {csp_script_src}; "
+                f"script-src-elem {csp_script_src}; "
+                "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com; "
+                "font-src 'self' data: https://cdnjs.cloudflare.com https://fonts.gstatic.com; "
+                "img-src 'self' data: blob: https:; "
+                "connect-src 'self' https://static.cloudflareinsights.com https://cloudflareinsights.com https://www.google.com/recaptcha/ https:;"
+            )
+
+            response['Content-Security-Policy'] = csp_policy
         return response
